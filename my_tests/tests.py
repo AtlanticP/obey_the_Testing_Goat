bd9743278ee77029f0f17687597d4d8768454fbd
Yaromir <Yaromir>
2018-01-02 15:13:17 +0300

    added requirements.txt

---
+++ my_tests/tests.py
@@ -0,0 +1,20 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class MyTest(LiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_my_test(self):
+
+		# self.browser.get(self.live_server_url)
+		# print(self.browser.get(self.live_server_url))
+
+		self.browser.get(self.live_server_url)
+
+		self.assertIn('To-Do', self.browser.title)
