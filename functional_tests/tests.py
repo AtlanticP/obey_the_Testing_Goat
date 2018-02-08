92d62434715d3c1ee94e4f3c690422a6688f5260
Yaromir <Yaromir>
2018-02-08 09:22:43 +0300

    08.02.18 Splitted FT

---
+++ functional_tests/tests.py
@@ -0,0 +1,166 @@
+import time
+import os
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.firefox.options import Options
+from unittest import skip
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):  
+        
+        options = Options()
+        options.add_argument('--headless')
+        self.browser = webdriver.Firefox(firefox_options=options) #, executable_path='~/django/sites/geckodriver')
+        print(' Firefox Headless Browser Invoked')
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+            print(self.live_server_url)
+    
+    def tearDown(self):  
+        self.browser.quit()
+
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (WebDriverException, AssertionError) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for_row_in_list_table(self, row_text):
+        
+        MAX_WAIT = 3
+        start_time = time.time()
+        
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+
+                return
+
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('Home page', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        inputbox.send_keys('Buy peacock feathers')  
+        inputbox.send_keys(Keys.ENTER)  
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')  
+        inputbox.send_keys(Keys.ENTER)  
+        
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+       
+        # She notices that her list has a unique URL
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/list/.+')
+        # Satisfied, she goes back to sleep
+
+    
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser.quit()
+       
+        options = Options()
+        options.add_argument('--headless')
+        self.browser = webdriver.Firefox(firefox_options=options)
+         
+         # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        time.sleep(3)
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+         
+        self.fail('Finish the test!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Buy peacock feathers')  
+        inputbox.send_keys(Keys.ENTER)  
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/list/.+')
+
+class ItemValidationTest(NewVisitorTest, LiveServerTestCase):
+
+    # @skip
+    def test_cannot_add_empty_list_items(self):
+
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The home page refreshes, and there is an error message saying 
+        # that list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.find_element_by_css_selector('.hass_error').text,
+            "You can't have an empty list item"
+        ))
+        # She tries again with some text for the item, which now works
+        
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can coorect it by filling some text in
+        self.fail('write me')
+        
+
