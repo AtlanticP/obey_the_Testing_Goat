f02ea50858ce0c2d005603351668b46afc433515
skok <skok@gmail.com>
2018-03-19 22:17:50 +0300

    03.19 something

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import List, Item
+from django.core.exceptions import ValidationError
 
 class ItemAndListModelsTest(TestCase):
 
@@ -15,3 +16,11 @@ class ItemAndListModelsTest(TestCase):
         self.assertEqual(saved_items[0].text, 'Item one')
         self.assertEqual(saved_items[1].text, 'Item two')
 
+    def test_cannot_save_an_empty_list_item(self):
+
+        lst = List.objects.create()
+        item = Item.objects.create(text='', list=lst)
+
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
