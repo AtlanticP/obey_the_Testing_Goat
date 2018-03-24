a16d2d28a9bcede3a3d5a6effbbba9784412bacf
skok <skok@gmail.com>
2018-03-24 17:10:39 +0300

    13. Refactor hard-coded URLs out of templates

--- lists/urls.py
+++ lists/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('list/<int:list_id>/add_item/', views.add_item, name = 'add_item'),
-	path('list/<int:list_id>/', views.list_view, name = 'list'),
+	path('list/<int:list_id>/', views.list_view, name = 'list_view'),
 	path('list/new/', views.new_list, name = 'new_list'),
     path('', views.home_page, name = 'home'),
 ]
