797d199c5bda41ed2c98e1df0568e7a628394031
office ubuntu <davidovnv@gmail.com>
2018-04-09 15:51:41 +0300

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -3,20 +3,14 @@ from lists.models import List, Item
 from django.core.exceptions import ValidationError
 from unittest import skip
 
-class ItemAndListModelsTest(TestCase):
+class ItemModelsTest(TestCase):
 
   def test_cannot_save_an_empty_list_item(self):
-
     lst = List.objects.create()
     item = Item.objects.create(text='', list=lst)
-
     with self.assertRaises(ValidationError):
       item.save()
-      item.full_clean()
-
-  def test_get_absolute_url(self):
-    lst = List.objects.create()
-    self.assertEqual(lst.get_absolute_url(), f'/list/{lst.id}/')
+      item.full_clean() 
 
   def test_duplicate_items_are_invalid(self):
     lst = List.objects.create()
@@ -24,6 +18,7 @@ class ItemAndListModelsTest(TestCase):
     with self.assertRaises(ValidationError):
       item = Item(list=lst, text='bla')
       item.full_clean()
+      # item.save() it gives n IntegrityError instead of a ValidationError
 
   def test_CAN_save_same_item_to_different_lists(self):
     lst1 = List.objects.create()
@@ -43,29 +38,7 @@ class ItemAndListModelsTest(TestCase):
     item.save()
     self.assertIn(item, lst.item_set.all())
 
-  def test_saves_and_retrieves_data_from_db(self):
-
+class ListModelsTest(TestCase):
+  def test_get_absolute_url(self):
     lst = List.objects.create()
-    Item.objects.create(text='Item one', list=lst)
-    Item.objects.create(text='Item two', list=lst)
-
-    saved_items = Item.objects.all()
-
-    self.assertEqual(Item.objects.count(), 2)
-    self.assertEqual(saved_items[0].text, 'Item one')
-    self.assertEqual(saved_items[1].text, 'Item two')
-
-  def test_list_ordering(self):
-    lst1 = List.objects.create()
-    item1 = Item.objects.create(list=lst1, text='i1')
-    item2 = Item.objects.create(list=lst1, text='item2')
-    item3 = Item.objects.create(list=lst1, text='i3')
-
-    self.assertEqual(
-      list(Item.objects.all()),
-      [item1, item2, item3]
-    )
-
-  def test_string_representation(self):
-    item = Item(text='some text')
-    self.assertEqual(str(item), 'some text')
+    self.assertEqual(lst.get_absolute_url(), f'/list/{lst.id}/')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -42,16 +42,20 @@ class ListViewTest(TestCase):
     self.assertContains(response, 'Item one')
     self.assertContains(response, 'Item two')
   
-  @skip  
   def test_can_save_a_POST_request_to_an_existing_list(self):
-    self.post_valid_input()
+    lst = List.objects.create()
+    response = self.client.post(f'/list/{lst.id}/', data={
+      'text': 'A new item for an existing list'
+    })
     self.assertEqual(Item.objects.count(), 1)
     new_item = Item.objects.first()
     self.assertEqual(new_item.text, 'A new item for an existing list')
     self.assertEqual(new_item.list, lst)
         
   def test_passes_correct_list_to_template(self):
-    pass
+    lst = List.objects.create()
+    response = self.client.get(f'/list/{lst.id}/')
+    self.assertEqual(response.context['list'], lst)
 
   def test_redirects_after_POST_request_to_an_existing_list(self):
     lst = List.objects.create()
